24183af7522cf4061bbf4fccb760b543d942317b
donotr <donovan.tobar16@gmail.com>
2023-10-03 21:06:14 -0600

    fix: se edito la direccion de screenhome en tabbar

--- lib/navigations/Tabbar.dart
+++ lib/navigations/Tabbar.dart
@@ -1,6 +1,6 @@
 import 'package:e_commerce/screens/profile/Screen_Profile_Login.dart';
 import 'package:e_commerce/screens/screen_categories.dart';
-import 'package:e_commerce/widgets/screenhomewidgets.dart';
+import 'package:e_commerce/screens/home/screenhomewidgets.dart';
 import 'package:flutter/material.dart';
 
 class Tabbar extends StatefulWidget {
