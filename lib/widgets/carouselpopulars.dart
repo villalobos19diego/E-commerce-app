2ef0c00f9689e372b6fa6cab0d101c75c0c98c61
donotr <donovan.tobar16@gmail.com>
2023-10-04 19:26:01 -0600

    fix: se corrigieron los nombres de los carousel español-ingles

---
+++ lib/widgets/carouselpopulars.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:e_commerce/data/data_carousel_pop.dart';
+import 'package:e_commerce/models/carouselpop_model.dart';
+import 'package:e_commerce/widgets/show_carousel_pop.dart';
+
+class CarruselViewPops extends StatelessWidget {
+  const CarruselViewPops({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 30,
+        ),
+        CarouselSlider.builder(
+          itemCount: carouselImagesPopular.length,
+          itemBuilder: ((context, index, realIndex) {
+            //Dice que no se usa pero si se usa, NO TOCAR
+            final carouselImagePopular = carouselImagesPopular[index];
+            return CardImages(
+              carouselImages: carouselImagesPopular[index],
+            );
+          }),
+          options: CarouselOptions(
+            height: 400.0,
+            autoPlay: true,
+            autoPlayCurve: Curves.easeInOut,
+            enlargeCenterPage: true,
+            autoPlayInterval: const Duration(seconds: 5),
+            scrollDirection: Axis.horizontal,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class CardImages extends StatelessWidget {
+  final CarouselPops carouselImages;
+  const CardImages({super.key, required this.carouselImages});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 300,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: InkWell(
+          onTap: () {
+            carouselImages.copy();
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => ShowCarouselPop(carouselImages: carouselImages,)));
+          },
+          child: FadeInImage(
+            placeholder: const AssetImage("assets/images/34.jpeg"),
+            image: AssetImage(carouselImages.image),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/carouseltops.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:e_commerce/data/data_carousel_top.dart';
+import 'package:e_commerce/models/carouseltop_model.dart';
+import 'package:e_commerce/widgets/show_carousel_top.dart';
+
+class CarruselViewTops extends StatelessWidget {
+  const CarruselViewTops({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 30,
+        ),
+        CarouselSlider.builder(
+          itemCount: carouselImagesTops.length,
+          itemBuilder: ((context, index, realIndex) {
+            //Dice que no se usa pero si se usa, NO TOCAR
+            final carouselImageTops = carouselImagesTops[index];
+            return CardImages(
+              carouselImages: carouselImagesTops[index],
+            );
+          }),
+          options: CarouselOptions(
+            height: 400.0,
+            autoPlay: true,
+            autoPlayCurve: Curves.easeInOut,
+            enlargeCenterPage: true,
+            autoPlayInterval: const Duration(seconds: 4),
+            scrollDirection: Axis.horizontal,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class CardImages extends StatelessWidget {
+  final CarouselTops carouselImages;
+  const CardImages({super.key, required this.carouselImages});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 300,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: InkWell(
+          onTap: () {
+            carouselImages.copy();
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => ShowCarouselTop(carouselImages: carouselImages,)));
+
+          },
+          child: FadeInImage(
+            placeholder: const AssetImage("assets/images/34.jpeg"),
+            image: AssetImage(carouselImages.image),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
