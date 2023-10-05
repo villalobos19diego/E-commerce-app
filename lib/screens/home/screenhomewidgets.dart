405ddac87bcf9946ed32df399cab3a4142176eed
donotr <donovan.tobar16@gmail.com>
2023-10-04 19:24:42 -0600

    fix: se cambio de carpeta, se corrigieron los nombres

--- lib/screens/home/screenhomewidgets.dart
+++ lib/screens/home/screenhomewidgets.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-
-import 'package:e_commerce/widgets/barrabusqueda.dart';
-import 'package:e_commerce/widgets/carruseles.dart';
-import 'package:e_commerce/widgets/carruseles2.dart';
+import 'package:e_commerce/widgets/searchbar.dart';
+import 'package:e_commerce/widgets/gridcategories.dart';
+import 'package:e_commerce/widgets/carouselPopulars.dart';
+import 'package:e_commerce/widgets/carouselTops.dart';
 import 'package:e_commerce/widgets/drawer.dart';
 
 class ScreenHome extends StatefulWidget {
@@ -14,65 +14,71 @@ class ScreenHome extends StatefulWidget {
 
 class _ScreenHomeState extends State<ScreenHome> {
   @override
- @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        drawer: const DrawerWidget(),
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('Lingerie App',style: TextStyle(color: Color.fromARGB(255, 225,190, 231)),),
-          backgroundColor: const Color.fromARGB(216, 107, 45, 117),
-          iconTheme: IconThemeData(color: Color.fromARGB(255, 225,190, 231)), // Cambio del color del icono del Drawer aquí
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(
-                Icons.shopping_cart,
-                color: Color.fromARGB(255, 225,190, 231),
-              ),
-              onPressed: () {
-                // hara algo
-              },
-            )
-          ],
-        ),
-        
-        body: 
-        SingleChildScrollView(          
-          child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            //BARRA BUSQUEDA
-            const OurSearchBar(),
-            //TEXTO DEL CARRUSEL
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 30, 10, 5),
-              child: const Text(
-                'Most Popular',
-                style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
-              ),
+      resizeToAvoidBottomInset: false,
+      drawer: const DrawerWidget(),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Lingerie App',style: TextStyle(color: Color.fromARGB(255, 225,190, 231)),),
+        backgroundColor: const Color.fromARGB(216, 107, 45, 117),
+        iconTheme: IconThemeData(color: Color.fromARGB(255, 225,190, 231)), // Cambio del color del icono del Drawer aquí
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.shopping_cart,
+              color: Color.fromARGB(255, 225, 190, 231),
             ),
-            //EMPIEZAN LOS CARRUSELES
-            const CarruselView(),
-            //TEXTO DEL CARRUSEL
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 30, 10, 5),
-              child: const Text(
-                'Top Sales',
-                style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
-              ),
+            onPressed: () {
+              // hara algo
+            },
+          )
+        ],
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        physics: const ClampingScrollPhysics(),
+        children: [
+          //BARRA BUSQUEDA
+          const OurSearchBar(),
+          //TEXTO DEL CARRUSEL POPULARES
+          Container(
+            padding: const EdgeInsets.fromLTRB(10, 30, 10, 5),
+            child: const Text(
+              'Most Popular',
+              style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
             ),
-            //EMPIEZA CARRUSEL 2
-            const CarruselView2(),
-          // prueba imagenes de colecciones
-          ],        
-        ),
-        ),
-        
-        
-      );
+          ),
+
+          //EMPIEZAN LOS CARRUSELES POPULARES
+          const CarruselViewPops(),
+          
+
+          //TEXTO DEL CARRUSEL
+          Container(
+            padding: const EdgeInsets.fromLTRB(10, 30, 10, 5),
+            child: const Text(
+              'Top Sales',
+              style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
+            ),
+          ),
+          //EMPIEZA CARRUSEL 2
+          const CarruselViewTops(),
+          Container(
+            padding: const EdgeInsets.fromLTRB(10, 40, 10, 40),
+          ),
+
+
+          //GRIDBUTTON QUE MUESTRA LAS CATEGORIAS
+
+          const GridButton(),
+        ],
+      ),
+    );
   }
 }
+
 
   
 
