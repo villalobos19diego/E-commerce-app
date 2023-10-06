479a114fd0908839bb2dc7f39555fac7cf72788d
Will1184 <brandonwilliammg@gmail.com>
2023-10-06 03:43:03 -0600

    fix: se modifico main para hacer uso de firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:e_commerce/firebase_options.dart';
 import 'package:e_commerce/navigations/Tabbar.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
