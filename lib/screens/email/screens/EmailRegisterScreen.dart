15ccc2c5afe61084289e862f0c00f1b8cdec6574
Diego Villalobos <Diegovillalobos503@gmail.com>
2023-10-08 12:21:06 -0600

    //separe el codigo  una contiene el registro del... otra la barra de navegacion la otra recibe los parametros para unir se agrego una nueva barra de navegacion el cual contiene una animacion al momento del cambio de menus

---
+++ lib/screens/email/screens/EmailRegisterScreen.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool _showPassword = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+
+      body: SizedBox(
+        width: size.width,
+        height: size.height,
+        child: Stack(
+          children: [
+
+            Positioned(
+
+              left: 16,
+              top: 16,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.pink,
+
+                      // Your profile image here
+                    ),
+
+                    const SizedBox(height: 16),
+                    IconButton(
+                      icon: const Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.pinkAccent,
+                      ),
+                      onPressed: () {
+                        // Add logic for navigating back
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: size.width * 0.85,
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(87, 239, 133, 168),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(height: size.height * 0.05, child: const ColoredBox(color: Colors.cyanAccent)),
+                      TextFormField(
+                        validator: (value) => _validateEmail(value),
+                        controller: emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          hintText: "New Email",
+                          isDense: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          prefixIcon: const Icon(
+                            Icons.email, // Your icon here
+                            color: Colors.black,
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                        ),
+                      ),
+                      SizedBox(height: size.height * 0.03),
+                      TextFormField(
+                        obscureText: !_showPassword,
+                        validator: (value) => _validatePassword(value),
+                        controller: passwordController,
+                        keyboardType: TextInputType.visiblePassword,
+                        decoration: InputDecoration(
+                          hintText: "Current Password",
+                          isDense: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          prefixIcon: const Icon(
+                            Icons.lock, // Your icon here
+                            color: Colors.black,
+                          ),
+                          suffixIcon: IconButton(
+                            color: Colors.lightBlueAccent,
+                            icon: Icon(
+                              _showPassword ? Icons.visibility : Icons.visibility_off,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _showPassword = !_showPassword;
+                              });
+                            },
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                        ),
+                      ),
+                      SizedBox(height: size.height * 0.06),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: ElevatedButton(
+                              onPressed: _handleRegister,
+                              style: ElevatedButton.styleFrom(
+                                primary: const Color.fromARGB(255, 255, 70, 131),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 40,
+                                  vertical: 15,
+                                ),
+                              ),
+                              child: const Text(
+                                "Email Change",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your email';
+    }
+    // You can add more email validations here if needed
+    return null;
+  }
+
+  String? _validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your password';
+    }
+    // You can add more password validations here if needed
+    return null;
+  }
+
+  void _handleRegister() {
+    if (_formKey.currentState!.validate()) {
+      final email = emailController.text;
+      final password = passwordController.text;
+      print('Email: $email');
+      print('Password: $password');
+      // You can perform registration logic here
+    }
+  }
+}
+
+
+
+
+
+
