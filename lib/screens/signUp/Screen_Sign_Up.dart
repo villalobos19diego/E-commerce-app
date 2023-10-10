06abf87b8ec9e7cfda5a3d41260256e1ae14abd2
Will1184 <brandonwilliammg@gmail.com>
2023-10-09 22:04:52 -0600

    feat: se agrego el objeto de la clase authservice para hacer uso de sus metodos y crear usuarios con firebase

--- lib/screens/signUp/Screen_Sign_Up.dart
+++ lib/screens/signUp/Screen_Sign_Up.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/config/services/auth_service.dart';
 import 'package:e_commerce/config/utils.dart';
 import 'package:e_commerce/navigations/Tabbar.dart';
 import 'package:e_commerce/screens/login/Screen_Login.dart';
@@ -11,6 +12,7 @@ class ScreenSignUp extends StatefulWidget {
 }
 
 class _ScreenLoginState extends State<ScreenSignUp> {
+  final AuthService _authService = AuthService();
   Utils utils = Utils();
   final _formKey = GlobalKey<FormState>();
   bool passToggle = true;
@@ -26,15 +28,16 @@ class _ScreenLoginState extends State<ScreenSignUp> {
 
   void _submit() {
     if (_formKey.currentState!.validate()) {
-      _usernamefieldController.clear();
-      _passwordfieldController.clear();
-      _emailfieldController.clear();
-      _formKey.currentState!.deactivate();
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text("Great!"),
-        ),
-      );
+      _authService.signInHandlerEmail(
+          _emailfieldController.text, _passwordfieldController.text);
+      if (mounted) {
+        _emailfieldController.clear();
+        _passwordfieldController.clear();
+        _emailfieldController.clear();
+        _formKey.currentState!.deactivate();
+        Navigator.push(
+            context, MaterialPageRoute(builder: (_) => const Tabbar()));
+      }
     }
   }
 
@@ -60,145 +63,142 @@ class _ScreenLoginState extends State<ScreenSignUp> {
         backgroundColor: const Color.fromARGB(216, 107, 45, 117),
       ),
       floatingActionButton: Padding(
-          padding: const EdgeInsets.only(top: 100), 
-          child: IconButton(
-            onPressed: () {
-             Navigator.pop(
-                  context,MaterialPageRoute(
-                    builder: (context) => const Tabbar()
-                    )
-                  );
-            },
-            icon: const Icon(Icons.arrow_back, color:Color.fromARGB(216, 107, 45, 117),size: 40),
-          ),
+        padding: const EdgeInsets.only(top: 100),
+        child: IconButton(
+          onPressed: () {
+            Navigator.pop(context,
+                MaterialPageRoute(builder: (context) => const Tabbar()));
+          },
+          icon: const Icon(Icons.arrow_back,
+              color: Color.fromARGB(216, 107, 45, 117), size: 40),
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
       body: Wrap(
         children: [
           Padding(
             padding: const EdgeInsets.only(
-            top: 200.0, bottom: 0.0, right: 50.0, left: 50.0),
+                top: 200.0, bottom: 0.0, right: 50.0, left: 50.0),
             child: Form(
               key: _formKey,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   textForm(
-                    "Username",
-                    const Color.fromARGB(255, 141, 140, 140),
-                    16.0,
-                    "Ingrese su Username",
-                    const Color.fromARGB(255, 121, 120, 120),context,
-                    icon: const Icon(Icons.person,
-                    color: Color.fromARGB(216, 107, 45, 117)),
-                    borderSide: BorderSide.none,
-                    borderRadius: BorderRadius.circular(8.0),
-                    filled: true,
-                    colorsFill: const Color.fromARGB(255, 248, 237, 250),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "No puede dejar este campo vacio";
+                      "Username",
+                      const Color.fromARGB(255, 141, 140, 140),
+                      16.0,
+                      "Ingrese su Username",
+                      const Color.fromARGB(255, 121, 120, 120),
+                      context,
+                      icon: const Icon(Icons.person,
+                          color: Color.fromARGB(216, 107, 45, 117)),
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(8.0),
+                      filled: true,
+                      colorsFill: const Color.fromARGB(255, 248, 237, 250),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "No puede dejar este campo vacio";
                         }
                         return utils.validateUsername(value);
-                        },
-                        focusNode: fieldUsername,
-                        textEditingController: _usernamefieldController,
-                        onfieldSubmitted: (value) {
-                          FocusScope.of(context).requestFocus(fieldEmail);
-                          }),
-                  const SizedBox(
-                    height: 16.0,
-                    ),
-                  textForm(
-                    "Email",
-                    const Color.fromARGB(255, 148, 131, 131),
-                    16.0,
-                    "Ingrese su Email",
-                    const Color.fromARGB(255, 158, 158, 158),
-                    context,
-                    icon: const Icon(Icons.email,
-                    color: Color.fromARGB(216, 107, 45, 117)),
-                    borderSide: BorderSide.none,
-                    borderRadius: BorderRadius.circular(8.0),
-                    filled: true,
-                    colorsFill: const Color.fromARGB(255, 248, 237, 250),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "No puede dejar este campo vacio";
-                        }
-                        return utils.validateEmail(value);
-                        },
-                    focusNode: fieldEmail,
-                    textEditingController: _emailfieldController,
-                    onfieldSubmitted: (value) {
-                      FocusScope.of(context).requestFocus(fieldPassword);
+                      },
+                      focusNode: fieldUsername,
+                      textEditingController: _usernamefieldController,
+                      onfieldSubmitted: (value) {
+                        FocusScope.of(context).requestFocus(fieldEmail);
                       }),
                   const SizedBox(
                     height: 16.0,
-                    ),
+                  ),
                   textForm(
-                    "Password", 
-                    const Color.fromARGB(255, 141, 140, 140),
-                    16.0, 
-                    "Ingrese su Password", 
-                    Colors.grey, context,
-                    icon: const Icon(Icons.lock,
-                    color: Color.fromARGB(216, 107, 45, 117)),
-                    borderSide: BorderSide.none,
-                    borderRadius: BorderRadius.circular(8.0),
-                    filled: true,
-                    colorsFill: const Color.fromARGB(255, 248, 237, 250),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "No puede dejar este campo vacio";
+                      "Email",
+                      const Color.fromARGB(255, 148, 131, 131),
+                      16.0,
+                      "Ingrese su Email",
+                      const Color.fromARGB(255, 158, 158, 158),
+                      context,
+                      icon: const Icon(Icons.email,
+                          color: Color.fromARGB(216, 107, 45, 117)),
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(8.0),
+                      filled: true,
+                      colorsFill: const Color.fromARGB(255, 248, 237, 250),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "No puede dejar este campo vacio";
+                        }
+                        return utils.validateEmail(value);
+                      },
+                      focusNode: fieldEmail,
+                      textEditingController: _emailfieldController,
+                      onfieldSubmitted: (value) {
+                        FocusScope.of(context).requestFocus(fieldPassword);
+                      }),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  textForm("Password", const Color.fromARGB(255, 141, 140, 140),
+                      16.0, "Ingrese su Password", Colors.grey, context,
+                      icon: const Icon(Icons.lock,
+                          color: Color.fromARGB(216, 107, 45, 117)),
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(8.0),
+                      filled: true,
+                      colorsFill: const Color.fromARGB(255, 248, 237, 250),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "No puede dejar este campo vacio";
                         }
                         return utils.validatePassword(value);
+                      },
+                      suffixInkwell: InkWell(
+                        onTap: () {
+                          setState(() {
+                            passToggle = !passToggle;
+                          });
                         },
-                        suffixInkwell: InkWell(
-                          onTap: () {
-                            setState(() {
-                              passToggle = !passToggle;
-                              });
-                              },
-                              child: Icon(
-                                passToggle ? Icons.visibility : Icons.visibility_off,
-                                color: const Color.fromARGB(216, 107, 45, 117)),
-                                ),
+                        child: Icon(
+                            passToggle
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            color: const Color.fromARGB(216, 107, 45, 117)),
+                      ),
                       isPassword: passToggle,
                       focusNode: fieldPassword,
                       textAction: TextInputAction.done,
                       textEditingController: _passwordfieldController,
                       onfieldSubmitted: (value) {
                         _submit();
-                        }),
-                    const SizedBox(height: 30.0),
-                    ElevatedButton(
+                      }),
+                  const SizedBox(height: 30.0),
+                  ElevatedButton(
                       focusNode: buttonFocus,
                       style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(280.0, 50.0),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0))),
-                          onPressed: () {
-                            _submit();
-                            },
-                        child: const Text("Sign Up")),
-                    const SizedBox(
-                      height: 16.0,
-                      ),
-                    const Text(" Already have an account?"),
-                    TextButton(
+                          minimumSize: const Size(280.0, 50.0),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0))),
                       onPressed: () {
-                        Navigator.push(
+                        _submit();
+                      },
+                      child: const Text("Sign Up")),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  const Text(" Already have an account?"),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => const ScreenLogin()));
-                            },
-                       child: const Text("Log In"),
-                       ),
+                              builder: (context) => const ScreenLogin()));
+                    },
+                    child: const Text("Log In"),
+                  ),
                 ],
               ),
             ),
-          )  
+          )
         ],
       ),
     );
