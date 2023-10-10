84954288f5d42f63e5ec3d6666cf6bdbf27b37f7
Will1184 <brandonwilliammg@gmail.com>
2023-10-09 21:06:34 -0600

    fix: se modifico el nombre de la clase y se le agregaron un constructor ademas de cambiarle el nombre a widget a usar <ScreenFormEmail>

--- lib/screens/email/widgets/Screen_Email.dart
+++ lib/screens/email/widgets/Screen_Email.dart
@@ -1,11 +1,13 @@
+import 'package:e_commerce/screens/email/widgets/Screen_Form_Email.dart';
 import 'package:flutter/material.dart';
-import 'package:untitled/my_home_page.dart';
-class MyApp extends StatelessWidget {
+class ScreenEmail extends StatelessWidget {
+  const ScreenEmail({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: ScreenFormEmail(),
     );
   }
 }
