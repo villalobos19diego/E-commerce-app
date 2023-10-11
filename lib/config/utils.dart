f52eb47c2972aec228732a2e1ad8a5bfef305864
Will1184 <brandonwilliammg@gmail.com>
2023-10-10 18:14:46 -0600

    feat: se agrego un metodo para comprobar si los dos password son iguales

--- lib/config/utils.dart
+++ lib/config/utils.dart
@@ -1,46 +1,52 @@
-
-class Utils{
-
+class Utils {
   String? validateUsername(String username) {
     final RegExp regex = RegExp(r'^[a-zA-Z0-9_]{3,}$');
-    if(username.length < 3){
+    if (username.length < 3) {
       return "Nombre de usuario debe ser mayor a 3";
     }
-    if(!regex.hasMatch(username)){
+    if (!regex.hasMatch(username)) {
       return "Nombre de usuario no valido";
     }
-      return null;
+    return null;
   }
-  
+
+  String? validatePasswordWithPassword(String password, String repeatPassword) {
+    if (repeatPassword != password) {
+      return "Las contraseñas no coinciden";
+    }
+    return null;
+  }
+
   String? validatePassword(String password) {
     final RegExp regexLowerCase = RegExp(r'^(?=.*?[a-z])');
     final RegExp regexUpperCase = RegExp(r'^(?=.*?[A-Z])');
     final RegExp regexNumber = RegExp(r'^(?=.*?[0-9])');
     final RegExp regexSpecial = RegExp(r'^(?=.*?[!@#\$&*~])');
 
-    if(password.length < 8){
+    if (password.length < 8) {
       return "Contraseña debe ser mayor a 8 Digitos";
     }
-    if(!regexLowerCase.hasMatch(password)){
+    if (!regexLowerCase.hasMatch(password)) {
       return "Debe contener una letra Minuscula";
     }
-    if(!regexUpperCase.hasMatch(password)){
+    if (!regexUpperCase.hasMatch(password)) {
       return "Debe contener una letra Mayuscula";
     }
-    if(!regexNumber.hasMatch(password)){
+    if (!regexNumber.hasMatch(password)) {
       return "Debe contener un numero";
     }
-    if(!regexSpecial.hasMatch(password)){
+    if (!regexSpecial.hasMatch(password)) {
       return "Debe contener letras especiales";
     }
     return null;
   }
-  
+
   String? validateEmail(String email) {
-    final RegExp regex = RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$');
-    if(!regex.hasMatch(email)){
+    final RegExp regex =
+        RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$');
+    if (!regex.hasMatch(email)) {
       return "Ingrese Un Email Valido";
-      }
-      return null;
+    }
+    return null;
   }
 }
