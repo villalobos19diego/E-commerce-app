b1b8f9e6e34bef8b39398601d758d9900418dbb4
Byron-Gomez <byronxgomez127@gmail.com>
2023-10-18 09:26:49 -0600

    feat: si en caso da null la imagen que muestre un icono de persona

--- lib/screens/photo/photo_user.dart
+++ lib/screens/photo/photo_user.dart
@@ -6,7 +6,6 @@ import 'package:e_commerce/config/services/upload_image.dart';
 import 'package:e_commerce/navigations/Tabbar.dart';
 import 'package:flutter/material.dart';
 
-
 class PhotoUser extends StatefulWidget {
   const PhotoUser({super.key});
 
@@ -16,94 +15,98 @@ class PhotoUser extends StatefulWidget {
 
 class _PhotoUserState extends State<PhotoUser> {
   File? imagenToUpload;
-   
-  @override  
+
+  @override
   Widget build(BuildContext context) {
-     AuthService authService = AuthService();
+    AuthService authService = AuthService();
     final image = authService.getPhoto();
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: const Icon(
-              Icons.person,
-              size: 50,
-              color: Color.fromARGB(255, 225, 190, 231),
-            ),
-            onPressed: () {},
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.person,
+            size: 50,
+            color: Color.fromARGB(255, 225, 190, 231),
           ),
-          title: const Align(
-            alignment: Alignment.bottomLeft,
-            child: Text(
-              "Log in",
-              style: TextStyle(color: Color.fromARGB(255, 225, 190, 231)),
-            ),
-          ),
-          backgroundColor: const Color.fromARGB(216, 107, 45, 117),
+          onPressed: () {},
         ),
-        floatingActionButton: Padding(
-          padding: const EdgeInsets.only(top: 100),
-          child: IconButton(
-            onPressed: () {
+        title: const Align(
+          alignment: Alignment.bottomLeft,
+          child: Text(
+            "Log in",
+            style: TextStyle(color: Color.fromARGB(255, 225, 190, 231)),
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(216, 107, 45, 117),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(top: 100),
+        child: IconButton(
+          onPressed: () {
             Navigator.pop(context);
-             Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (_) => const Tabbar()));
-            },
-            icon: const Icon(Icons.arrow_back,
-                color: Color.fromARGB(216, 107, 45, 117), size: 40),
-          ),
+            Navigator.push(
+                context, MaterialPageRoute(builder: (_) => const Tabbar()));
+          },
+          icon: const Icon(Icons.arrow_back,
+              color: Color.fromARGB(216, 107, 45, 117), size: 40),
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
-        body: Center(
-          child: Column(children: [
-            const SizedBox(
-              height: 100,
-            ),
-            image != null
-                ? Container(
-                    margin: const EdgeInsets.all(10),
-                    height: 200,
-                    decoration:
-                        BoxDecoration(borderRadius: BorderRadius.circular(40)),
-                    child: Image.network(image),
-                  )
-                :Container(
-                    margin: const EdgeInsets.all(10),
-                    height: 200,
-                    decoration:
-                        BoxDecoration(borderRadius: BorderRadius.circular(40)),
-                    child: Image.file(imagenToUpload!),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+      body: Center(
+        child: Column(children: [
+          const SizedBox(
+            height: 100,
+          ),
+          image != null
+              ? Container(
+                  margin: const EdgeInsets.all(10),
+                  height: 200,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(40)),
+                  child: Image.network(image),
+                )
+                // si no hay imagen que muestre un icono de persona 
+              : Container(
+                  margin: const EdgeInsets.all(10),
+                  height: 200,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(40)),
+                  child: Icon(
+                    Icons.person,
+                    size: 200,
+                    color: Color.fromARGB(255, 225, 190, 231),
                   ),
-            const SizedBox(
-              height: 100,
-            ),
-            ElevatedButton(
-              child: const Text("Seleccionar Imagen"),
-              onPressed: () async {
-                final imagen = await getImage();
-                setState(() {
-                  imagenToUpload = File(imagen!.path);
-                });
-              },
-            ),
-            ElevatedButton(
-              child: const Text("Subir Imagen"),
-              onPressed: () async {
-                if (imagenToUpload == null) {
-                  return;
-                }
-                final uploaded = await uploadImage(imagenToUpload!);
-                if (uploaded) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("Imagen Subida")));
-                }else{                  
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("Error al subir Imagen")));
-                }
-              },
-            ),
-          ]),
-        ));
+                ),
+          const SizedBox(
+            height: 100,
+          ),
+          ElevatedButton(
+            child: const Text("Seleccionar Imagen"),
+            onPressed: () async {
+              final imagen = await getImage();
+              setState(() {
+                imagenToUpload = File(imagen!.path);
+              });
+            },
+          ),
+          ElevatedButton(
+            child: const Text("Subir Imagen"),
+            onPressed: () async {
+              if (imagenToUpload == null) {
+                return;
+              }
+              final uploaded = await uploadImage(imagenToUpload!);
+              if (uploaded) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text("Imagen Subida")));
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text("Error al subir Imagen")));
+              }
+            },
+          ),
+        ]),
+      ),
+    );
   }
 }
