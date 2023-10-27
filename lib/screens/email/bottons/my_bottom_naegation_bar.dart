09b20e292aea6c15185bfa0ee7f6938cf781b94e
Admin <diegovillalobos503@gmail.com>
2023-10-27 12:29:32 -0600

    feat :      se agrego  los colores y se agrego iconbutton

--- lib/screens/email/bottons/my_bottom_naegation_bar.dart
+++ lib/screens/email/bottons/my_bottom_naegation_bar.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/navigations/Tabbar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -28,11 +29,16 @@ class _UpdateEmailScreenState extends State<UpdateEmailScreen> {
           return;
         }
 
+
+        await Future.delayed( const  Duration(seconds: 2));
+        Navigator.pop(context);
+         Navigator.pushReplacement( context, MaterialPageRoute(builder:  (_) =>  const Tabbar()));
+
         // Llama al método updateEmailWithConfirmation
         await auth.currentUser!.verifyBeforeUpdateEmail(newEmail);
 
         // Muestra un mensaje al usuario
-        ScaffoldMessenger.of(context).showSnackBar(
+         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(
             content:
             Text('Se ha enviado un correo electrónico de confirmación'),
@@ -44,20 +50,65 @@ class _UpdateEmailScreenState extends State<UpdateEmailScreen> {
 
     // Crea una pantalla con el campo de entrada y el botón
     return Scaffold(
-      body: Center(
+      appBar: AppBar(
+        leading: IconButton(onPressed: () {
+          Navigator.of(context).pop();
+
+          }, icon: const  Icon(Icons.arrow_back),
+      ),
+      ),
+
+
+      body:
+
+
+
+
+
+
+      Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Actualiza tu correo electrónico'),
+
+            const Text('Actualiza tu correo electrónico',  style: TextStyle(
+              fontSize: 20, // Tamaño de fuente personalizado
+              fontWeight: FontWeight.bold, // Puedes personalizar el peso de la fuente
+              color:Color.fromARGB(216, 107, 45, 117),// Color de texto personalizado
+            ),
+            ),
+             const SizedBox(height: 20),
+
             TextField(
               controller: newEmailController,
-              decoration: const InputDecoration(
+              decoration:  InputDecoration(
                 hintText: 'Nuevo correo electrónico',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0), // Personalizar los bordes
+                ),
+                prefixIcon:  const Icon(Icons.email, color: Color.fromARGB(216, 107, 45, 117),
+                ),
+                contentPadding:  const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
               ),
+              style:  const TextStyle( fontSize: 16),
             ),
-            updateEmailButton,
+            const  SizedBox(height:  20),
+              ElevatedButton(onPressed: () {   updateEmailButton;
+              },
+
+
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all( const Color.fromARGB(216, 107, 45, 117)),
+              padding: MaterialStateProperty.all( const EdgeInsets.all(15.0),
+              ),
+
+              ),
+                child:
+                const Text('Actualizar Correo Eltronico', style: TextStyle(fontSize: 18,),),
+              )
+
           ],
         ),
+
       ),
     );
   }
