8463e1d052e9b2e3ad3c05378e9f6c0866a2eede
Will1184 <brandonwilliammg@gmail.com>
2023-11-05 23:35:26 -0600

    feat: se agrego screen para ver detalles de producto

---
+++ lib/screens/admin/producto/detalles_producto_page.dart
@@ -0,0 +1,15 @@
+import 'package:e_commerce/models/producto.dart';
+import 'package:flutter/material.dart';
+class DetalleProductoPage extends StatefulWidget {
+  const DetalleProductoPage({super.key, required Producto producto});
+
+  @override
+  State<DetalleProductoPage> createState() => _DetalleProductoPageState();
+}
+
+class _DetalleProductoPageState extends State<DetalleProductoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
