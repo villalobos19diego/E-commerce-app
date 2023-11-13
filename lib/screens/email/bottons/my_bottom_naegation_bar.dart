3cf042fd4f54f6ebebbbfe633d548011cbba6e33
Diego Villalobos <Diegovillalobos503@gmail.com>
2023-11-12 20:33:56 -0600

    feat:  implemento la nueva clase

--- lib/screens/email/bottons/my_bottom_naegation_bar.dart
+++ lib/screens/email/bottons/my_bottom_naegation_bar.dart
@@ -1,313 +1,202 @@
-import 'package:e_commerce/navigations/Tabbar.dart';
+
+
+
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:user_profile_avatar/user_profile_avatar.dart';
-class UpdateEmailScreen extends StatefulWidget {
+import '../../password/image/UserProfileImage.dart';
+import '../../profile/Screen_username.dart';
 
+
+class UpdateEmailScreen extends StatefulWidget {
   const UpdateEmailScreen({super.key});
 
   @override
-  State<UpdateEmailScreen> createState() => _UpdateEmailScreenState();
+  State<UpdateEmailScreen> createState() => _UpdateEmailScreenScreenState();
 }
 
-class _UpdateEmailScreenState extends State<UpdateEmailScreen> with SingleTickerProviderStateMixin {
-  // late AnimationController _controller;
-  // late Animation<double> _animation;
-  bool _showEmail =  false;
+class _UpdateEmailScreenScreenState extends State<UpdateEmailScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  bool _showPassword = false;
 
   @override
   void initState() {
     super.initState();
-    // _controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 300),);
-    // _animation = Tween(begin: 1.0, end: 1.2).animate(_controller);
-
+    _emailController.text = '';
   }
-  bool showIndications = false;
-  // Obtén la instancia de la autenticación de Firebase
-  final FirebaseAuth auth = FirebaseAuth.instance;
 
-  // Crea un campo de entrada para el nuevo correo electrónico
-  final TextEditingController newEmailController = TextEditingController();
-
-  //defnmos en  mensAje de entrada
-  final String confirmationMassage =
-      'Se ha enviado un correo electronico a tu nuevo correo electronico, ';
-
-
-
-
-
-
-
+  Widget _buildTopSection() {
+    return const Align(
+      alignment: Alignment.topRight,
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: UserProfileImage(
+          profileImageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2iscIvwOLGIff33hveao7z3fexyWW_zfGcg&usqp=CAU',
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    final Size size =  MediaQuery.of(context).size;
-    // Crea un botón para actualizar el correo electrónico
-    final ElevatedButton updateEmailButton = ElevatedButton(
-
-
-
-      onPressed: () async {
-        // Obtén el nuevo correo electrónico del campo de entrada
-
-        final String newEmail = newEmailController.text;
-        if (!Validator.isEmail(newEmail)) {
-          return;}
-
-        // Llama al método updateEmailWithConfirmation
-        await auth.currentUser!.verifyBeforeUpdateEmail(newEmail);
-        // Muestra un mensaje al usuario
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-
-            content:
-            Text('Se ha enviado un correo e lectrónico de confirmación',style:TextStyle( color: Colors.amber) ),
-          ),
-        );
-      },
-      style: ElevatedButton.styleFrom(
-
-        minimumSize: const Size(double.infinity, 25),
-
-        padding: const EdgeInsets.all(25),
-
-        backgroundColor: const Color.fromARGB(216, 107, 45, 117),
-      ),
-
-      child: const Text('Actualizar correo electrónico'  ),
-
-
-    );
-
-
-
-    MaterialApp(theme: ThemeData(
-     // scaffoldBackgroundColor: Colors.amber,
-    ),);
-
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
-
-
-
-      appBar:
-      AppBar(
-
-
-        backgroundColor:
-        const Color.fromARGB(216, 107, 45, 117),
-        leading: IconButton(onPressed: () {
-          Navigator.pushReplacement(
-
-              context, MaterialPageRoute(builder: (_)
-          => const Tabbar())); },
-          icon:  const Icon(Icons.arrow_back),
-
-        ),
-        flexibleSpace:  Row(
-
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [UserProfileAvatar
-
-
-            (avatarUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSumsFnCbTdA2Nbo2tKJxbowPN70kWltlRtyJOXhmCV&s',
-            radius: 20,
-            notificationCount: 10,
-            notificationBubbleTextStyle: const TextStyle(
-
-              fontSize: 5,
-              color:Color.fromARGB(216, 107, 45, 117),
-              fontWeight: FontWeight.bold
-            ),
-            avatarSplashColor: Colors.purple,
-            avatarBorderData: AvatarBorderData(
-
-              borderColor: const Color.fromARGB(216, 107, 45, 117),
-              borderWidth: 5.0,
-            ),
-
-
-            onAvatarTap:(){if (kDebugMode) {
-              //  print('Avatar Tapped...');
-            }
-           const TextStyle(
-              fontSize: 20,
-              color: Colors.amber,
-              fontWeight: FontWeight.bold,
-            );
-            Colors.purple;
-            radius: 80;
-            isActivityIndicatorSmall: false;
-            avatarBorderData: AvatarBorderData(
-              borderColor: Colors.amber,
-              borderWidth: 5.0,
-            );
-
-
-
-            },
-          ),
-          ],
-        ),
-
-
-      ),
-
-
-
-
-      body:
-
-
-      Center(
-        child:
-
-        Container(width: 400,
-          padding: const  EdgeInsets.all(20),
-          decoration: BoxDecoration(
-            //  color:   Colors.black,
-            borderRadius: BorderRadius.circular(8),
-            boxShadow: [
-
-              BoxShadow(
-                color:  Colors.white.withOpacity(0.5),spreadRadius: 5,
-                blurRadius: 7,
-                offset: const  Offset(0, 3),
-              ),
-
-            ],
-          ),
-          child:
-
-
-
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget> [
-              IconButton(onPressed: () {  setState(() {
-                showIndications = !showIndications;
-
-              });},
-                  icon:
-                  const Icon(Icons.info_outline,textDirection: TextDirection.ltr,
-
-                    color: Color.fromARGB(216, 107, 45, 1170),),
-                  tooltip: 'Presione el boton para ver las inicaiones'),
-
-              if(showIndications )   const
-              Text( ' Al darle click te envian un correo a tu nuevo correo',
-                textAlign: TextAlign.end,
-                style:TextStyle(
-                  color:  Color.fromARGB(216, 107, 45, 1170),
-                  fontSize:  14,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.italic,
-                  letterSpacing: 2,
-                  height: 1.5,
-
-                ),
-
-
-
-
-
-              ),
-//agregamos el campo de textfeld...
-              SizedBox(
-                //bajar mas el boton de presionar
-                height:  size.height  * 0.10,
-
-                width: double.infinity,
-
-
-
-                child:
-                TextFormField(
-                  obscureText: !_showEmail,
-                  controller: newEmailController,
-
-                  keyboardType: TextInputType.visiblePassword,
-
-                  cursorColor: const Color.fromARGB(216, 107, 45, 1170),
-                  decoration:  InputDecoration(
-                    isDense: true,
-
-
-                    focusedBorder:const OutlineInputBorder(
-                      borderSide: BorderSide(color: Color.fromARGB(
-                          255, 0, 243, 255),),
-                    ),
-                    floatingLabelStyle: TextField.materialMisspelledTextStyle,
-                    hintText: 'Nuevo  correo electronico',
-
-
-                    border: OutlineInputBorder( borderRadius: BorderRadius.circular(15),
-                    ),
-
-                    prefixIcon: const Icon(Icons.email, color:Color.fromARGB(216, 107, 45, 117),),
-                    suffixIcon: IconButton(onPressed: ()
-                    {  setState(() {
-                      _showEmail = !_showEmail;
-                    });
-                    }, icon:
-                    Icon(_showEmail ? Icons.visibility : Icons.visibility_off),
-
-                    ),
-                    contentPadding:
-                    const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+      body: Stack(
+        children: [
+          _buildTopSection(),
+          Center(
+            child: Container(
+              width: 450,
+              padding: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.white.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3),
                   ),
-                  validator: (value){
-                    if(value!.contains("@")  || value.contains(".")
-                        || RegExp("^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z]+")
-                            .hasMatch(value)){
-                      return "El correo electrónico no es valido";
-
-                    }
-                    return null;
-                  },
-
-                ),
-
+                ],
+                //color:Colors.white,
               ),
-              updateEmailButton,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
 
-            ],
+                  SizedBox(
+                    height: size.height * 0.10,
+                    width: double.infinity,
+                    child: TextField(
+                      obscureText: !_showPassword,
+                      controller: _emailController,
+                      cursorColor: const Color.fromARGB(216, 107, 45, 117),
+                      decoration: InputDecoration(
+                        isDense: true,
+                        focusedBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color.fromARGB(216, 107, 45, 117),
+                          ),
+                        ),
+                        floatingLabelStyle:
+                        TextField.materialMisspelledTextStyle,
+                        labelText: 'Ingresa Tu Nuevo  Correo',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        prefixIcon: const Icon(
+                          Icons.email,
+                          color: Color.fromARGB(216, 107, 45, 117),
+                        ),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _showPassword = !_showPassword;
+                            });
+                          },
+                          icon: Icon(
+                            _showPassword
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                          ),
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(
+                          vertical: 20,
+                          horizontal: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(
+                        MaterialPageRoute(
+                            builder:
+                                (context) =>
+                            const UpdateEmailScreen()
+                        ),
+                      );
+                    },
+                    child:
+                    const Text(
+                      '',
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 13,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: ()  async {
+         final _authService = _AuthServicea();
+    await _authService.updateEmailWithVerification(_emailController.text);
+
+
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(double.infinity, 25),
+                      padding: const EdgeInsets.all(23),
+                      backgroundColor:
+                      const Color.fromARGB(216, 107, 45, 117),
+                    ),
+                    child: const Text('Cambiar Correo'),
+                  ),
+                ],
+              ),
+            ),
           ),
+          Positioned(
+            top: 16,
+            left: 16,
+            child: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () {
 
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (_)
+                  => const ScreenProfileUsername(),
+                  ),
+                );
 
-        ),
+              },
+            ),
+          ),
+        ],
       ),
-
-
-
     );
-
-
-
-    // Crea una pantalla con el campo de entrada y el botón ...
-
   }
 }
 
+class _AuthServicea {
 
 
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  Future<void> sendVerificationEmail(String emailAddress) async {
+    try {
+      await _auth.sendPasswordResetEmail(email: emailAddress);
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
 
-
-
-
-
-
-
-class Validator {
-  static bool isEmail(String email) {
-
-    return RegExp(
-      //definimos la  caracteristicas  o los campos  que deben  incluir el email...
-        r'^[a-zA-Z0-9.!#$%&+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+$')
-        .hasMatch(email);
+  Future<void> updateEmailWithVerification(String newEmail) async {
+    try {
+      await _auth.currentUser!.verifyBeforeUpdateEmail(newEmail);
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
   }
 }
+
 
