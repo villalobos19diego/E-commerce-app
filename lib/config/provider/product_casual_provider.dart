8aa665fe7bf288985310493f51c0de6a6c718d29
Diego Villalobos <Diegovillalobos503@gmail.com>
2023-12-14 17:58:00 -0600

    feat: se implemento  nueva requirimiento

--- lib/config/provider/product_casual_provider.dart
+++ lib/config/provider/product_casual_provider.dart
@@ -9,14 +9,16 @@ class ProductCasualProvider extends  ChangeNotifier{
       price: 50.75,
       image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRDoEl6vJF_GhmGKpCuZvk7JWCYdiSH8_IsQ&usqp=CAU",
-      isAvailable:  false,
+      isAvailable:  true, 
+      sizes: ["M", "L", "XL","S"],
     ),
     ProductModel(
       name: "vestido casual",
       price: 21.99,
       image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUn2CvK76JYJSLFp27DdOLqLhVKOhxhJs7Jg&usqp=CAU",
-      isAvailable: true,
+      isAvailable: true, 
+      sizes: ["M", "L", "XL","S"],
     ),
     ProductModel(
       name: "SHEIN VCAY Vestido con estampado floral de espalda abierta con nudo fruncido tirantes",
@@ -24,21 +26,22 @@ class ProductCasualProvider extends  ChangeNotifier{
       image:
           "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcR7PNUQJ3iC9gtal9WDaHdi696WCHRq2AFaC5xDODfbetJraM5PBnb_h0cb2_gA2IBTG7EQzzmTc1BdhRez6DUKg30YRaHViv79m44oYFZKtn6R9SiIzmFHsw&usqp=CAc",
       isAvailable: true,
+      sizes: ["M", "L", "XL","S"],
     ),
     ProductModel(
       name: "SHEIN LUNE Blusa unicolor de manga obispo con fruncido",
       price: 0.00,
       image:
           "",
-      isAvailable: true,
+      isAvailable: true, sizes: ["M", "L", "XL","S"],
     ),
-    ProductModel(
-      name: "ropa casual",
-      price: 0.00,
-      image:
-          "",
-      isAvailable:  false,
-    )
+
   ];
   List<ProductModel> get casuals => _casual;
+
+  void selectedSize(int index, String size){
+    if(index >= 0 && index < _casual.length){
+      _casual[index].selectedSize = size;
+    }
+  }
 }
