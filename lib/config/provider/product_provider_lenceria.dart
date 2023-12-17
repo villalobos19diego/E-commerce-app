349120e54973b5c1c67e4c47c56640d6fd8baf5e
Diego Villalobos <Diegovillalobos503@gmail.com>
2023-12-17 17:45:43 -0600

    feat:   agrego la talla XXL

--- lib/config/provider/product_provider_lenceria.dart
+++ lib/config/provider/product_provider_lenceria.dart
@@ -9,14 +9,16 @@ final List<ProductModel> _lenceria = [
       image:
           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm8J3mQnyPV5m-iD60dZfCAZm7r37gtMIAlg&usqp=CAU',
       isAvailable: true,
-      sizes: ["M", "L", "XL","S"], description: '',
+      sizes: ["M", "L", "XL","S"],
+      description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius m',
     ),
     ProductModel(
       name: "lenceria estilo esclava ",
       price: 29.99,
       image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJDNvvhZoyUM4fH1ignBTIftTzcxT7KpfsXV5X1hQ8OvMJgnTZkXmbIpLHIDhzBOz3gJQ&usqp=CAU",
-      isAvailable: true, sizes: ["M", "L", "XL","S"], description: '',
+      isAvailable: true, sizes: ["M", "L", "XL","S"],
+      description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius m',
     ),
 
 
@@ -25,9 +27,15 @@ final List<ProductModel> _lenceria = [
       price: 29.99,
       image:
           "assets/images/4.jpeg",
-      isAvailable: true, sizes: ["M", "L", "XL","S"], description: '',
+      isAvailable: true, sizes: ["M", "L", "XL","S"],
+       description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius m',
     ),
   ];
   List<ProductModel> get lencerias => _lenceria;
+void selectSize(int index, String size) {
+  if (index >= 0 && index < _lenceria.length) {
+    _lenceria[index].selectedSize = size;
+  }
+}
 
 }
