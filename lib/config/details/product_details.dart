9b112b2f44a0367e3b9a4326c949df7ab7970acc
Diego Villalobos <Diegovillalobos503@gmail.com>
2023-12-17 11:44:59 -0600

    feat:  se  agrego la descripcion y otr ventana el cual muestra las tallas antes de enviar el producto al carrito

--- lib/config/details/product_details.dart
+++ lib/config/details/product_details.dart
@@ -1,105 +1,303 @@
+
 import 'package:e_commerce/config/cart/models/product_model.dart';
+import 'package:e_commerce/config/provider/cart_provider.dart';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:provider/provider.dart';
 
 class ProductDetailsScreen extends StatefulWidget {
   final ProductModel product;
 
-   const ProductDetailsScreen({super.key, required this.product });
+
+
+  const ProductDetailsScreen({super.key, required this.product });
 
   @override
-  State<ProductDetailsScreen> createState() => _ProductDetailsScreenState();
+  State<ProductDetailsScreen> createState()
+  => _ProductDetailsScreenState();
 }
-class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
+class _ProductDetailsScreenState extends
+State<ProductDetailsScreen> {
   late String selectedSize;
+
 
   @override
   void initState() {
     super.initState();
     // Inicializar la talla seleccionada con la primera talla disponible
-    selectedSize = widget.product.sizes.isNotEmpty ? widget.product.sizes[0] : "";
+    selectedSize =
+    widget.product.sizes.isNotEmpty ? widget.product.sizes[0] : "";
   }
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(
-              alignment: AlignmentDirectional.topStart,
-              children: [
-                Container(
-                  height: 300,
-                  width: double.infinity,
-                  child: Image.network(
-                    widget.product.image,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.arrow_back),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  color: Colors.black,
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+        child: Container(
+          margin: const EdgeInsets.only(top: 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+
+              Stack(
+                alignment: AlignmentDirectional.topStart,
                 children: [
-                  Text(
-                    widget.product.name,
-                    style: GoogleFonts.lato(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Precio: \$${widget.product.price.toString()}',
-                    style: const TextStyle(fontSize: 18, color: Colors.green),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Producto: ${widget.product.isAvailable ? 'Disponible' : 'No disponible'}',
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                  const SizedBox(height: 8),
-                  // Usar Wrap para mostrar botones redondos con las tallas disponibles
-                  Wrap(
-                    spacing: 8.0,
-                    children: widget.product.sizes.map((String size) {
-                      return ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            selectedSize = size;
-                          });
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: size == selectedSize ? Colors.amber : Colors.grey, // Color del botón
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20.0), // Radio de la esquina del botón
-                          ),
+
+
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: 400,
+
+                        width: double.infinity,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10.0),
+                        child: Image.network(
+                          widget.product.image,
+                          fit: BoxFit.cover,
                         ),
-                        child: Text(
-                          size,
-                          style: TextStyle(
-                            color: Colors.white
-                            , // Color del texto
-                          ),
                         ),
-                      );
-                    }).toList(),
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    color: Colors.purple,
+
                   ),
                 ],
               ),
-            ),
-          ],
+
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child:
+                    Container(
+                      margin: const EdgeInsets.only(top: 8),
+                      child: Text(
+                        widget.product.name,
+                        style: const TextStyle(
+                          color:Colors.purple,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        )
+                      ),
+                    ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Center(child:
+                    Text(  'Descripción:',
+                      style: TextStyle(
+                        color:Colors.purple,
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+
+                      ),
+                    ),
+                    ),
+                    const SizedBox(height: 8),
+                    Center(
+
+                      child:Text(widget.product.description, style:
+                      const TextStyle(fontSize:16, color:Colors.purple))
+                    ),
+
+                    const SizedBox(height: 8),
+                    Center(
+                       child:
+                    Container(
+                      margin: const EdgeInsets.only(top: 8),
+                      child: Text(
+                        'Precio: \$${widget.product.price.toString()}',
+                        style: const TextStyle(fontSize: 18, color: Colors.purple),
+                      ),
+                    ),
+                    ),
+                    const SizedBox(height: 8),
+                    Center(
+                      child: Container(
+                        margin: const EdgeInsets.only(top: 8),
+                        child: Text(
+                          'Producto:  '
+                              '${widget.product.isAvailable ? '|Disponible|' : '|No disponible|'}',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: widget.product.isAvailable ? Colors.green : Colors.red,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(height: 16),
+                    const Center(child:
+                    Text(  'Tallas Disponibles:',
+                      style: TextStyle(
+                        color:Colors.purple,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+
+                      ),
+                    ),
+                    ),
+                    const SizedBox(height: 8),
+
+                    // Usar Wrap para mostrar botones redondos con las tallas disponibles
+                   Center(
+                     child:
+                    Container(
+                      margin: const EdgeInsets.only(top: 15),
+                      child: Column(
+                        children: [
+
+                          Wrap(
+
+                            spacing: 8.0,
+                            children: widget.product.sizes.map(
+                                    (String size) {
+
+                              return ElevatedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    selectedSize = size;
+                                  });
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: size == selectedSize ? Colors.purple : Colors.grey, // Color del botón
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0), // Radio de la esquina del botón
+                                  ),
+                                ),
+                                child: Text(
+                                  size,
+                                  style: const TextStyle(
+                                    color: Colors.white
+                                    , // Color del texto
+                                  ),
+                                ),
+                              );
+                            }).toList(),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                   ),
+                     const SizedBox(height: 10),
+                    //Botón para agregar al carrito
+                    GestureDetector(
+                      onTap: () async {
+                        String? selectedSize = await showDialog<String>(
+                          context: context,
+                          builder: (BuildContext context)  {
+                            return AlertDialog(
+                              title: const  Text('Tallas:',
+                                  style:
+                              TextStyle(color:Colors.purple,   fontSize: 13, // Ajusta el tamaño del título según tus necesidades
+                                fontWeight: FontWeight.bold), ),
+                              backgroundColor:Colors.white,
+                              contentPadding:
+                              const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                              content: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: widget.product.sizes.map((size) {
+                                  return ListTile(
+                                    title: Text(size, style: const  TextStyle(
+                                      fontSize: 16, // Ajusta el tamaño del texto de la lista según tus necesidades
+                                    ),),
+                                    onTap: () {
+                                      Navigator.pop(context, size);
+                                    },
+
+                                  );
+                                }).toList(),
+                              ),
+                            );
+                          },
+                        );
+
+                        if (selectedSize != null) {
+                          context
+                              .read
+                          <CartProvider>()
+                              .addToCart
+                            (widget.product,
+                              selectedSize);
+                          ScaffoldMessenger
+                              .of(context)
+                              .showSnackBar(
+                            SnackBar(
+                              backgroundColor: Colors.purple.withOpacity(0.8),
+                              content: const Text(
+                                "Se Agregó al Carrito",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          );
+                        }
+                      },
+                      child:Center(
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 44),
+                        child: Container(
+                            padding: const  EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              color: Colors.purple,
+                              borderRadius: BorderRadius.
+                              circular(10),
+                            ),
+                        child:   Row (
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const Icon(
+                              Iconsax.bag,
+                              size: 14,
+                              color: Colors.black,
+                            ),
+                             const SizedBox(width:8),
+
+                            Center(
+                              child: Container(
+                                margin: const EdgeInsets.only(top: 8),
+                                child: const Text(
+                                  'Seleccione Talla',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                          ],
+                        ),
+                    ),
+                      ),
+                    ),
+                    ),
+
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+
+
 }
 
