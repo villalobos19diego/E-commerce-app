ccf24954c519e03b2afb7c1edad80d7d300778e3
Diego Villalobos <Diegovillalobos503@gmail.com>
2023-12-24 10:25:22 -0600

    fix:   se agrego   logica de envio de whatsapp  los detalles que enviara se  agrego future  void el cual maneja el estado del widget el cual contiene los lugares  que estan dispoibles

--- lib/config/details/product_details.dart
+++ lib/config/details/product_details.dart
@@ -1,24 +1,69 @@
 
+
+
+
+
 import 'package:e_commerce/config/cart/models/product_model.dart';
+
 import 'package:e_commerce/config/provider/cart_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProductDetailsScreen extends StatefulWidget {
   final ProductModel product;
+  final List<String> availableDeliveryLocations;
 
 
 
-  const ProductDetailsScreen({super.key, required this.product });
+  const ProductDetailsScreen({super.key, required this.product, required this.availableDeliveryLocations });
 
   @override
   State<ProductDetailsScreen> createState()
   => _ProductDetailsScreenState();
 }
+
 class _ProductDetailsScreenState extends
 State<ProductDetailsScreen> {
+
+
   late String selectedSize;
+
+  String selectedLocation = "Ubicaciones de Entrega";
+
+  Future<void> _showLocationDialog() async {
+    String? newLocation = await showDialog<String>(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: const Text('Seleccionar Ubicación'),
+          children: widget.product.availableDeliveryLocations.map((location) {
+            return SimpleDialogOption(
+              onPressed: () {
+                Navigator.pop(context, location);
+              },
+              child: Text(location),
+            );
+          }).toList(),
+        );
+      },
+    );
+
+    if (newLocation != null) {
+      setState(() {
+        selectedLocation = newLocation;
+      });
+    }
+  }
+
+
+
+
+
+
+
 
 
   @override
@@ -26,8 +71,14 @@ State<ProductDetailsScreen> {
     super.initState();
     // Inicializar la talla seleccionada con la primera talla disponible
     selectedSize =
-    widget.product.sizes.isNotEmpty ? widget.product.sizes[0] : "";
+    widget.product.sizes.isNotEmpty ?
+    widget.product.sizes[0] : "";
+
+
+
   }
+
+
 
   @override
   Widget build(BuildContext context)  {
@@ -54,10 +105,10 @@ State<ProductDetailsScreen> {
                         width: double.infinity,
                         child: ClipRRect(
                           borderRadius: BorderRadius.circular(10.0),
-                        child: Image.network(
-                          widget.product.image,
-                          fit: BoxFit.cover,
-                        ),
+                          child: Image.network(
+                            widget.product.image,
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
                     ],
@@ -80,17 +131,17 @@ State<ProductDetailsScreen> {
                   children: [
                     Center(
                       child:
-                    Container(
-                      margin: const EdgeInsets.only(top: 8),
-                      child: Text(
-                        widget.product.name,
-                        style: const TextStyle(
-                          color:Colors.purple,
-                          fontSize: 17,
-                          fontWeight: FontWeight.bold,
-                        )
+                      Container(
+                        margin: const EdgeInsets.only(top: 8),
+                        child: Text(
+                            widget.product.name,
+                            style: const TextStyle(
+                              color:Colors.purple,
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                            )
+                        ),
                       ),
-                    ),
                     ),
                     const SizedBox(height: 8),
                     const Center(child:
@@ -106,20 +157,20 @@ State<ProductDetailsScreen> {
                     const SizedBox(height: 8),
                     Center(
 
-                      child:Text(widget.product.description, style:
-                      const TextStyle(fontSize:16, color:Colors.purple))
+                        child:Text(widget.product.description, style:
+                        const TextStyle(fontSize:16, color:Colors.purple))
                     ),
 
                     const SizedBox(height: 8),
                     Center(
-                       child:
-                    Container(
-                      margin: const EdgeInsets.only(top: 8),
-                      child: Text(
-                        'Precio: \$${widget.product.price.toString()}',
-                        style: const TextStyle(fontSize: 18, color: Colors.purple),
+                      child:
+                      Container(
+                        margin: const EdgeInsets.only(top: 8),
+                        child: Text(
+                          'Precio: \$${widget.product.price.toString()}',
+                          style: const TextStyle(fontSize: 18, color: Colors.purple),
+                        ),
                       ),
-                    ),
                     ),
                     const SizedBox(height: 8),
                     Center(
@@ -127,7 +178,7 @@ State<ProductDetailsScreen> {
                         margin: const EdgeInsets.only(top: 8),
                         child: Text(
                           'Producto:  '
-                              '${widget.product.isAvailable ? '|Disponible•|' : '|No Disponible•|'}',
+                              '${widget.product.isAvailable ? 'Disponible•' : 'No Disponible•'}',
                           style: TextStyle(
                             fontSize: 14,
                             color: widget.product.isAvailable ? Colors.green : Colors.red,
@@ -138,9 +189,13 @@ State<ProductDetailsScreen> {
                       ),
                     ),
 
-                    const SizedBox(height: 16),
-                    const Center(child:
-                    Text(  'Tallas Disponibles:',
+
+
+
+
+                    const SizedBox(height: 8),
+
+                    const Text(  'Tallas Disponibles:',
                       style: TextStyle(
                         color:Colors.purple,
                         fontSize: 15,
@@ -148,51 +203,55 @@ State<ProductDetailsScreen> {
 
                       ),
                     ),
-                    ),
+
                     const SizedBox(height: 8),
 
                     // Usar Wrap para mostrar botones redondos con las tallas disponibles
-                   Center(
-                     child:
-                    Container(
-                      margin: const EdgeInsets.only(top: 15),
-                      child: Column(
-                        children: [
+                    Center(
+                      child:
+                      Container(
+                        margin: const EdgeInsets.only(top: 15),
+                        child: Column(
+                          children: [
 
-                          Wrap(
+                            Wrap(
 
-                            spacing: 20.0,
-                            children: widget.product.sizes.map(
-                                    (String size) {
+                              spacing: 20.0,
+                              children: widget.product.sizes.map(
+                                      (String size) {
 
-                              return ElevatedButton(
-                                onPressed: () {
-                                  setState(() {
-                                    selectedSize = size;
-                                  });
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: size == selectedSize ? Colors.purple : Colors.grey, // Color del botón
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20.0), // Radio de la esquina del botón
-                                  ),
-                                ),
-                                child: Text(
-                                  size,
-                                  style: const TextStyle(
-                                    color: Colors.white
-                                    , // Color del texto
-                                  ),
-                                ),
-                              );
-                            }).toList(),
-                          ),
-                        ],
+                                    return ElevatedButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          selectedSize = size;
+                                        });
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: size == selectedSize ? Colors.purple : Colors.grey, // Color del botón
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(20.0), // Radio de la esquina del botón
+                                        ),
+                                      ),
+                                      child: Text(
+                                        size,
+                                        style: const TextStyle(
+                                          color: Colors.white
+                                          , // Color del texto
+                                        ),
+                                      ),
+                                    );
+                                  }).toList(),
+                            ),
+                          ],
+                        ),
                       ),
+
                     ),
 
-                   ),
-                     const SizedBox(height: 10),
+
+
+
+                    const SizedBox(height: 10),
                     //Botón para agregar al carrito
                     widget.product.isAvailable ?
                     GestureDetector(
@@ -202,10 +261,10 @@ State<ProductDetailsScreen> {
                           builder: (BuildContext context)   {
                             return AlertDialog(
                               title: const  Text('Tallas:',
-                                  style:
-                              TextStyle(color:Colors.purple,
-                                  fontSize: 13, // Ajusta el tamaño del título según tus necesidades
-                                fontWeight: FontWeight.bold), ),
+                                style:
+                                TextStyle(color:Colors.purple,
+                                    fontSize: 13, // Ajusta el tamaño del título según tus necesidades
+                                    fontWeight: FontWeight.bold), ),
                               backgroundColor:Colors.white,
                               contentPadding:
                               const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
@@ -228,15 +287,13 @@ State<ProductDetailsScreen> {
                         );
 
                         if (selectedSize != null) {
-                          context
-                              .read
-                          <CartProvider>()
-                              .addToCart
-                            (widget.product,
-                              selectedSize);
-                          ScaffoldMessenger
-                              .of(context)
-                              .showSnackBar(
+                          context.read<CartProvider>().addToCart(
+                            widget.product,
+                            selectedSize,
+                            selectedLocation, // Agregar selectedLocation
+                          );
+
+                          ScaffoldMessenger.of(context).showSnackBar(
                             SnackBar(
                               backgroundColor: Colors.purple.withOpacity(0.8),
                               content: const Text(
@@ -248,46 +305,154 @@ State<ProductDetailsScreen> {
                             ),
                           );
                         }
+
                       },
                       child:Center(
-                      child: Padding(
-                        padding: const EdgeInsets.only(bottom: 44),
-                        child: Container(
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 44),
+                          child: Container(
                             padding: const  EdgeInsets.all(8),
                             decoration: BoxDecoration(
                               color: Colors.purple,
                               borderRadius: BorderRadius.
                               circular(10),
                             ),
-                        child:   Row (
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            const Icon(
-                              Iconsax.bag,
-                              size: 10,
-                              color: Colors.black,
-                            ),
-                             const SizedBox(width:15),
+                            child:   Row (
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                const Icon(
+                                  Iconsax.bag,
+                                  size: 10,
+                                  color: Colors.black,
+                                ),
+                                const SizedBox(width:15),
 
-                            Center(
-                              child: Container(
-                                margin: const EdgeInsets.only(top: 8),
-                                child: const Text(
-                                  'Seleccione Talla',
-                                  style: TextStyle(
-                                    color: Colors.black,
+                                Center(
+                                  child: Container(
+                                    margin: const EdgeInsets.only(top: 8),
+                                    child: const Text(
+                                      'Seleccione Talla',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ),
 
-                          ],
+
+
+
+                              ],
+                            ),
+                          ),
                         ),
-                    ),
                       ),
-                    ),
                     ) :
                     Container(),
+
+                    const SizedBox(height: 8),
+                    ElevatedButton(
+                      onPressed: ()  async {
+                        // Obtener la información del producto y la talla seleccionada
+                        String productInfo = 'Producto: ${widget.product.name}\n';
+                        productInfo += 'Descripción: ${widget.product.description}\n';
+                        productInfo += 'Precio: \$${widget.product.price.toString()}\n';
+                        productInfo += 'Talla seleccionada: $selectedSize\n';
+                        productInfo += 'Entrega En: $selectedLocation\n';
+
+                        //  ByteData bytes = await rootBundle.load(widget.product.image);
+                        //   List<int> imageData = bytes.buffer.asUint8List();
+
+
+                        //   await Share.file(
+                        //   'Producto: ${widget.product.name}',
+                        //   'product.jpg',
+                        //   imageData,
+                        //   'image/jpeg',
+                        //   text: productInfo,
+                        // );
+
+                        // Lanzar la función para abrir WhatsApp con la información del producto
+                        await _launchWhatsApp(productInfo);
+                      },
+                      child: Center(
+                        child: Padding(
+                          padding:  const EdgeInsets.only(bottom:20),
+                          child:Container(
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              color: Colors.purple,
+                              borderRadius: BorderRadius.circular(10),
+
+
+                            ),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                const Icon(
+                                  Icons.call_made,
+                                  size: 20,
+                                  color: Colors.black,
+                                ),
+                                const SizedBox(width: 15),
+                                Center(
+                                  child:Container(
+                                    margin: const EdgeInsets.only(top: 8),
+                                    child: const Text(
+                                      'Solicitar por WhatsApp',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+
+                            ),
+
+
+
+
+
+                          ),
+                        ),
+                      ),
+                    ),
+
+
+
+
+
+
+
+
+
+
+
+                     const SizedBox(height: 8),
+                       const Text(
+                'Puntos Disponibles:',
+                  style: TextStyle(
+                     color: Colors.purple,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                   ),
+                  ),
+                 const SizedBox(height: 8),
+
+              // Muestra las ubicaciones de entrega disponibles
+              GestureDetector(
+                onTap: _showLocationDialog,
+                child: Row(
+                  children: [
+                    const Icon(Icons.location_on),
+                    const SizedBox(width: 8),
+                    Text(selectedLocation),
+                  ],
+                ),
+              ),
+
+
                   ],
                 ),
               ),
@@ -297,7 +462,24 @@ State<ProductDetailsScreen> {
       ),
     );
   }
+  _launchWhatsApp(String message) async {
+    // Número de teléfono para el destinatario
+    String phoneNumber = '+50360670744';
+
+
+    // Crear el enlace de WhatsApp
+    String url = 'https://wa.me/$phoneNumber?text=${Uri.encodeComponent(message)}';
+
+    // Abrir WhatsApp
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      // Manejar el caso en que WhatsApp no está instalado
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('WhatsApp no está instalado en tu dispositivo.'),
+      ));
+    }
+  }
 
 
 }
-
